678c57c268d2132e44046818755681297e8cf612
cuongnc0211 <cuongnguyenfu@gmail.com>
2020-10-29 23:11:06 +0700

    add template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "books#index"
+  root "books#home"
+
+  resources :books, only: :index
 
   namespace :admin do
     root "dashboard#home"
